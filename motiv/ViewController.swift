439f2d4f25bcd88ef33d3439432d6a9381b2d721
Samantha Muller <1samanthamuller@gmail.com>
2020-07-28 11:33:47 -0400

    project

---
+++ motiv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  motiv
+//
+//  Created by Jodi Muller on 7/28/20.
+//  Copyright © 2020 Samantha Muller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
